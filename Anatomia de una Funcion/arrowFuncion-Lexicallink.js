97c66f278a3c0628c6a9662bc3c390aec804320d
Alejandro Bautista <cristianlopezbautista@gmail.com>
2025-02-17 21:50:05 -0600

    anatomia de una funcion, arror y closure

--- Anatomia de una Funcion/arrowFuncion-Lexicallink.js
+++ Anatomia de una Funcion/arrowFuncion-Lexicallink.js
@@ -15,7 +15,7 @@ const newGreeting = (name) =>{
       return `Hi, ${name}`
 }
 
-//Arrow Function - implicit return
+//Arrow Function - implicit, no lleva un return y ni llaves
 //no lleva parentesis a menos que se agreguen mas parametros
 const newGreetingImplicit = name =>`Hi, ${name}`
 const newGreetingImplicitWithTwoParameters = (name,lastname) => `hi ${name} ${lastname}`
@@ -34,7 +34,21 @@ const functionalCharacter = {
     /*
     En las arrow functions, el contexto de this se determina léxicamente, lo que significa que heredan el valor de this del contexto donde fueron definidas. A diferencia de las funciones tradicionales, que crean su propio contexto, las arrow functions no tienen su propio this. Esto provoca que al intentar usar this dentro de una arrow function, se refiera al this del ámbito externo en el que se definió, lo que puede resultar en un comportamiento inesperado si se espera que this haga referencia al objeto en el que se invoca la función.
 
+    Las Arrow Function son útiles porque permiten:
 
+Escribir métodos más concisos
+Simplificar una línea de código para que sea más legible
+Aprovechar las características de retorno implícito y el no uso de paréntesis
+Olvidarse de manejar el contexto this
+Definir de manera compacta una función convencional
+Eliminar las llaves y la palabra return si la función tiene solamente una sentencia que devuelve un valor
+Reducir el código aún más utilizando parámetros
+
+Cuando se usa una función flecha como método de un objeto, el this dentro de la función flecha no apunta al objeto que invocó el método. En cambio, el valor de this se hereda del contexto de ejecución en el que se definió la función flecha. Esto significa que this se refiere al this del entorno circundante (en el que se creó la función), y no puede ser cambiado con métodos como call, apply o bind.
+
+Exacto, la arrow function no posee su propio contexto "this". En su lugar, capturan el "this" del contexto de donde fueron definidas, en otras palabras, si "this" no está definido en ese contexto, será "undefined".
+
+Para lograr que la arrow function use el "this" del objeto "finctionalCharacter" debe acceder directamente a finctionalCharacter:
      */
     const finctionalCharacter = {
     name: 'Peter Parket',
---
+++ Anatomia de una Funcion/closure.js
@@ -0,0 +1,64 @@
+/*
+Closure
+Funcion que tiene accesso a variables de un ambito eterno, inclsuo despues de que esa funcion haya termiando de ejecutarse
+
+Ambito Lexico: cada vez que se declara una funcion, crea su propio ambito lexico, y puede acceder a las variables dentro de ese ambito y a las variables en ambitos superiores
+
+Se pueden crear funciones flexibles pero ser evitativo para el uso de memoria
+tener distintos tipos de contextos
+ */
+
+function outerFunction(){
+    let outerVariable = "I am from outer function"
+    
+    function innerFunction(){
+        console.log(outerVariable)
+    }
+    return innerFunction
+
+}
+
+const closureExample = outerFunction()
+closureExample() // I am from outer function
+
+// Segundo Ejemplo
+
+
+function createCounter(){
+    let count = 0;
+    return function(){
+     count++;
+     console.log(count);   
+    }
+}
+
+const counterA= createCounter()
+counterA()
+counterA()
+//tendra probolemas de memoria
+//los closures no debe de exceder el uso de memoria
+const counterB = createCounter()
+counterB()
+
+
+//Tercer Ejemplo
+
+/**
+
+La función inner accede a las variables definidas en el ámbito de la función outer gracias al concepto de closure. Aunque outer no reciba parámetros, las variables definidas dentro de ella, como other_variable, son accesibles por inner debido a que inner se define dentro del scope de outer. Esto se relaciona con el ámbito léxico, donde una función puede acceder a las variables de su contexto de creación, incluso después de que la función outer haya terminado de ejecutarse.
+
+
+
+ */
+function outer(){
+    let message = 'Hello , '
+    function inner(name){
+    console.log(message + ' ' + name)
+    }
+    return inner
+}
+const closureA = outer()
+const closureB = outer()
+
+closureA('ALEJANDRO')
+closureB('JUAN')
