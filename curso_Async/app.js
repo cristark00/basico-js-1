dba1d1b16467081c688438b55ef1048f753141bc
Alejandro Bautista <cristianlopezbautista@gmail.com>
2025-03-13 22:33:02 -0600

    add the app.js

--- curso_Async/app.js
+++ curso_Async/app.js
@@ -18,7 +18,7 @@
     //set time out ayudara para darle un tiempo para que el llamando del api se pueda resolver
     setTimeout(()=>{
         let operationSuccessful = true;
-        if(operationSuccessful){
+        if(!operationSuccessful){
             resolve("La operación se realizó con éxito");
         } else{
             reject("Fallo la operacion");
@@ -26,3 +26,34 @@
     }, 2000);
     
  })
+
+ promise
+ .then((successMessage) => {
+    console.log(successMessage);
+ })
+ .catch((errorMessage) => {
+    console.log(errorMessage);
+    })
+
+
+//Async and Await
+/*
+El resultado es el mismo devuelve una promesa
+las promesas existieron despues de los callbacks
+async y await es una forma de escribir de manera mas limpia
+
+ */
+
+//REVISAR LA CLASE DE FUNDAMENTOS DE JAVASCRIPT ASYNC,
+
+const walkDog = new Promise ((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('You walkd the dog 🐕');
+    }, 1500);
+})
+
+const cleanKitche= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('You cleaned the kitchen 🧹');
+    }, 2500);
+})
